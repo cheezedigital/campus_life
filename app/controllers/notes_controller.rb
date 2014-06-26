f8fed2bdbb6d734c2c73d54a021755a8a787d580
Justin Veirs <justinveirs@gmail.com>
2014-06-26 09:57:04 -0600

    show, index.html. testing controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,13 @@
 class NotesController < ApplicationController
+
+  def index
+    @notes = Note.all
+  end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  
+
 end
--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -7,12 +7,23 @@ RSpec.describe NotesController, :type => :controller do
         @notes = create_list(:note, 3)
       end
       it 'displays a collection of notes' do
-        get :index 
+        get :index #this preforms actions
+        expect(response).to be_success
+        expect(assigns(:notes).count).to eq 3 #return value from instance variable
+        expect(response).to render_template('index')#render index
       end
     end
 
-    describe '#show' do
-
+    describe '#show', :focus do
+      before do
+        @note = create(:note, text: 'my little note')
+      end
+      it 'displays a single note' do
+        get :show, id: @note.id #how we get show action
+        expect(response).to be_success
+        expect(assigns(:note).text).to eq 'my little note'
+        expect(response).to render_template('show')
+      end
     end
 
     describe '#new' do
